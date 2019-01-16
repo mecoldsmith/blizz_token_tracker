819832afc16b47c0fbba6ec890d1d2ee45bf2ddb
Matthew <mecoldsmith@gmail.com>
2019-01-15 23:17:37 -0500

    Updated for API changes
    Updated the urls and options for the new blizzard developer api.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const http = require('https');
 
 const mysql = require("mysql");
 const config = require("./config.js");
+
 var currentPrice, lastUpdate;
 var chartData, chartLabels;
 
@@ -16,32 +17,30 @@ app.get('/', function(req, res) {
 	res.render('index.ejs');
 });
 
-
 function getAllTokenInfo() {
 	//This will call each Token info in turn;
-	//for now needs to have access tokens added to code, should use oath in the end
-	getTokenInfo("us.api.battle.net","/data/wow/token/?namespace=dynamic-us&locale=en_US&access_token="); //us
-	getTokenInfo("eu.api.battle.net","/data/wow/token/?namespace=dynamic-eu&locale=en_GB&access_token="); //eu
-	getTokenInfo("kr.api.battle.net","/data/wow/token/?namespace=dynamic-kr&locale=ko_KR&access_token="); //kr
-	getTokenInfo("tw.api.battle.net","/data/wow/token/?namespace=dynamic-tw&locale=zh_TW&access_token="); //tw
-	//getTokenInfo("",""); //China probably isnt happening now.. wont let me log in
+	getTokenInfo("us.api.blizzard.com","/data/wow/token/index?namespace=dynamic-us&locale=en_US&access_token=");//&region=US"); //us
+	getTokenInfo("eu.api.blizzard.com","/data/wow/token/index?namespace=dynamic-eu&locale=en_eu&access_token="); //eu
+	getTokenInfo("kr.api.blizzard.com","/data/wow/token/index?namespace=dynamic-kr&locale=en_kr&access_token="); //kr
+	getTokenInfo("tw.api.blizzard.com","/data/wow/token/index?namespace=dynamic-tw&locale=en_tw&access_token="); //tw
 }
 
 function getTokenInfo(url, path) {
 	var options = {
 	  "method": "GET",
-	  "hostname": url,//"us.api.battle.net",
+	  "hostname": url,
 	  "port": null,
-	  "path": path,//"/data/wow/token/?namespace=dynamic-us&locale=en_US&access_token=",
+	  "path": path, 
 	  "headers": {
 		"cache-control": "no-cache",
-		"postman-token": ""// if used from postman needs the token
+		"postman-token": ""
 	  }
+	  
 	};
 	
 	var req = http.request(options, function (res) {
 	  var chunks = [];
-	
+	  //res.setEncoding('UTF-8');
 	  res.on("data", function (chunk) {
 		chunks.push(chunk);
 	  });
@@ -50,28 +49,29 @@ function getTokenInfo(url, path) {
 		var body = Buffer.concat(chunks);
 		console.log(body.toString());
 		body = JSON.parse(body);
-		if(body._links.self.href == "https://us.api.battle.net/data/wow/token/?namespace=dynamic-us"){
+		console.log('body: ' + body);
+
+		if(body._links.self.href == "https://us.api.blizzard.com/data/wow/token/?namespace=dynamic-us"){
 			parseTokenInfo(body,"Token_Data_US");
 		}
-		else if(body._links.self.href == "https://kr.api.battle.net/data/wow/token/?namespace=dynamic-kr") {
+		else if(body._links.self.href == "https://kr.api.blizzard.com/data/wow/token/?namespace=dynamic-kr") {
 			parseTokenInfo(body,"Token_Data_KR");
 		}
-		else if(body._links.self.href == "https://eu.api.battle.net/data/wow/token/?namespace=dynamic-eu"){
+		else if(body._links.self.href == "https://eu.api.blizzard.com/data/wow/token/?namespace=dynamic-eu"){
 			parseTokenInfo(body,"Token_Data_EU");
 		}
-		
-		else if(body._links.self.href == "https://tw.api.battle.net/data/wow/token/?namespace=dynamic-tw") {
+		else if(body._links.self.href == "https://tw.api.blizzard.com/data/wow/token/?namespace=dynamic-tw") {
 			parseTokenInfo(body, "Token_Data_TW");
 		}
 	  });
 	});
+	
 	req.end();	
 }
 function parseTokenInfo(data, table_name) {
 	//now will take a second param of table_name which will be added as a parameter to the sql strings
 		currentPrice = data.price;
-		lastUpdate = data.last_updated;
-		//lastUpdate = new Date(dateFix).toISOString().replace(/T/, ' ').replace(/\..+/, '');
+		lastUpdate = data.last_updated_timestamp;
 		currentPrice = currentPrice / 10000;
 
 		console.log('test price: ' + currentPrice.toLocaleString() + " test date: " + lastUpdate + " table name: " + table_name);
@@ -80,17 +80,21 @@ function parseTokenInfo(data, table_name) {
 		
 		con.connect(function(err) {
 			if(err)  {
+				console.log(err);
 				return console.error(err.message);
 			}
 			else{
 				console.log("connected");
+				//'call storeToken(' + curentPrice + ',' + lastUpdate + ',\'' + table_name + '\')'
 				let sql = 'Call storeToken(' + currentPrice + ',' + lastUpdate + ',\'' + table_name + '\')';
 				con.query(sql, function(err, result) {
 					if(err) {
+						console.log(err);
 						return console.error(err.message);
 					}
 					else{
 						console.log(result[0]);
+						return console.log(result[0]);
 					}
 					con.end();
 				});
@@ -119,7 +123,7 @@ function getDataAndEmit(socket) {
 					chartArr.push(JSON.stringify(result[1]));
 					chartArr.push(JSON.stringify(result[2]));
 					chartArr.push(JSON.stringify(result[3]));
-					socket.emit('token_info', chartArr);				
+					socket.emit('token_info', chartArr);//chartData);				
 				}
 				con.end();
 			});
@@ -131,10 +135,12 @@ io.sockets.on('connection', function(socket, price, date) {
 	getAllDataAndEmit(socket);
 	setInterval(function() {
 		getAllDataAndEmit(socket);
-	}, 910000);	
+	}, 910000);
+	
+	
 });
 
 getAllTokenInfo();
 setInterval(getAllTokenInfo, 900000);
 
-server.listen(9292);
+server.listen(5858);
